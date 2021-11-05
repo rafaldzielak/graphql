6a9a8380677545a81b6139ac3701a9e05bb422ab
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-11-05 16:27:39 +0100

    List of all books for author & author and books query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,9 +1,12 @@
-import { GraphQLSchema, GraphQLString, GraphQLObjectType, GraphQLID, GraphQLInt } from "graphql";
+import { GraphQLSchema, GraphQLString, GraphQLObjectType, GraphQLID, GraphQLInt, GraphQLList } from "graphql";
 
 const books = [
     { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
     { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2" },
+    { name: "The Hero of Ages", genre: "Fantasy", id: "4", authorId: "2" },
     { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" },
+    { name: "The Colour of Magic", genre: "Fantasy", id: "5", authorId: "3" },
+    { name: "The Light Fantastic", genre: "Fantasy", id: "6", authorId: "3" },
 ];
 
 const authors = [
@@ -31,6 +34,10 @@ const AuthorType = new GraphQLObjectType({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
         age: { type: GraphQLInt },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve: (parent, args) => books.filter((book) => book.authorId === parent.id),
+        },
     }),
 });
 
@@ -46,6 +53,14 @@ const RootQuery = new GraphQLObjectType({
             type: AuthorType,
             args: { id: { type: GraphQLID } },
             resolve: (parent, args) => authors.find((author) => author.id === args.id),
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve: () => books,
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            resolve: () => authors,
         },
     },
 });

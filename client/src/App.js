12d43cc252e2e7a3a3e964f9b4b1fad11211af86
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-11-07 14:28:01 +0100

    Client: Apollo setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import BookList from "./components/BookList";
 
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from "@apollo/client";
+
+// Apoll client setup
+
+const client = new ApolloClient({ uri: "http://localhost:5000/graphql", cache: new InMemoryCache() });
+
 function App() {
   return (
-    <div id='main'>
-      <h1>Reading list</h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div id='main'>
+        <h1>Reading list</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 

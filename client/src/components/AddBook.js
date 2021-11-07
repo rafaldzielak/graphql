8ea793cec40b477e776f593e2a633d37e05b45df
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-11-07 15:21:00 +0100

    Client: Add Book mutation with useMutation

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,9 +1,14 @@
-import React from "react";
-import { useQuery } from "@apollo/client";
-import { getAuthorsQuery } from "../queries/queries";
+import React, { useEffect, useState } from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import { addBookMutation, getAuthorsQuery } from "../queries/queries";
 
 const AddBook = () => {
   const { loading, error, data } = useQuery(getAuthorsQuery);
+
+  const [name, setName] = useState("");
+  const [genre, setGenre] = useState("");
+  const [authorId, setAuthorId] = useState("");
+  const [addBookMut, { dataMutation }] = useMutation(addBookMutation);
 
   const displayAuthors = () => {
     if (loading) return <option>Loading authors</option>;
@@ -14,24 +19,31 @@ const AddBook = () => {
     ));
   };
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addBookMut({
+      variables: { name, genre, authorId },
+    });
+  };
+
   return (
-    <form id='add-book'>
+    <form id='add-book' onSubmit={onSubmit}>
       <div className='field'>
         <label>Book name:</label>
-        <input type='text' />
+        <input type='text' onChange={(e) => setName(e.target.value)} value={name} />
       </div>
 
       <div className='field'>
         <label>Genre:</label>
-        <input type='text' />
+        <input type='text' onChange={(e) => setGenre(e.target.value)} value={genre} />
       </div>
 
       <div className='field'>
         <label>Author:</label>
-        <select>{displayAuthors()}</select>
+        <select onChange={(e) => setAuthorId(e.target.value)}>{displayAuthors()}</select>
       </div>
 
-      <button></button>
+      <button>+</button>
     </form>
   );
 };

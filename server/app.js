ee74f17c4d2d28493e5e741f19a84af8881c7c0a
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-11-07 14:46:00 +0100

    Client: make request from react & render books

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import { bookSchema } from "./schema/schema.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 app.use("/graphql", graphqlHTTP({ schema: bookSchema, graphiql: true }));
 
